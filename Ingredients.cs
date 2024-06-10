c4e88cde05f4fca7fd3cf9300112ac17979e0e7c
JustinXRG <justin1405xrg@gmail.com>
2024-06-10 15:16:49 +0700

    tes

--- Ingredients.cs
+++ Ingredients.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace ProjectOOP
@@ -8,6 +9,8 @@ namespace ProjectOOP
     public class Ingredients
     {
         private string name;
+
+        public string display;
         private int id;
     }
 }
