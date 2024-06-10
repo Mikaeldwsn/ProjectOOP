632282c5277aa3a47aef1b5e9601d1f02d89dc52
ChristopherKayne <christopherkayne05@gmail.com>
2024-06-10 14:33:17 +0700

    tambah

---
+++ Beverages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectOOP
+{
+    public class Beverages
+    {
+        private bool isCold;
+        private string size;
+
+        public bool IsCold { get => isCold; set => isCold = value; }
+        public string Size { get => size; set => size = value; }
+
+
+    }
+}
