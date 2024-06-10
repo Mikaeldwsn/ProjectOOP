27cfc52868375dd9f059f68602d47e49b46765c6
Mikaeldwsn <mikaels2005@gmail.com>
2024-06-10 14:10:12 +0700

    class customer

---
+++ Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace ProjectOOP
+{
+    public class Customer
+    {
+        private string name;
+        private Image picture;
+        private string type;
+
+        public string Name { get => name; set => name = value; }
+        public Image Picture { get => picture; set => picture = value; }
+        public string Type 
+        { 
+            get => type;
+            set 
+            { 
+                type = value; 
+            } 
+        }
+    }
+}
