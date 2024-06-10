950fc96c9ece0e6249cd8981e7ea0976532b61d3
JustinXRG <justin1405xrg@gmail.com>
2024-06-10 14:35:09 +0700

    Class Food

---
+++ Food.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectOOP
+{
+    public class Food:Items
+    {
+        private List<Ingredients> listOfIngredients = new List<Ingredients>();
+
+
+    }
+}
